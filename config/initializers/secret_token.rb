889c30a3d8730b313c3576b8e4093d3137f0d75f
Paul Sutcliffe <paul@kosmyka.com>
2014-01-14 23:50:57 -0500

    ignore secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jemimagen::Application.config.secret_token = '74911349e74a262e0958a7d99c3131bf8e587894548ad8fc189f9b452e60e432fc8145826ddc05d1dbd94cd00ce90f1d5f2168f69f65895bc686d9dbfe500ff8'
