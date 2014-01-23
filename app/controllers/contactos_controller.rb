66dc06a2b2111d2a88ccc15da8ee5738e6d3896a
adrielpinzas <adriel@kosmyka.com>
2014-01-23 16:54:13 -0500

    Implementado el mailer

--- app/controllers/contactos_controller.rb
+++ app/controllers/contactos_controller.rb
@@ -1,2 +1,18 @@
 class ContactosController < InheritedResources::Base
+
+  def create
+    create! do |success, failure|
+      success.html do
+        ContactoMailer.mensaje_recibido(@contacto).deliver
+
+        flash[:notice] = "Tu mensaje fue enviado con exito."
+        redirect_to new_contacto_path
+      end
+      failure.html do
+        flash[:notice] = "No ha sido enviado el mensaje."
+        render :accion => "new"
+      end
+    end
+  end
+
 end
